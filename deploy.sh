ad288eefe95bf7857f01fa93bf97509693b6cafb
Shivani <uic.shivanijaswal@gmail.com>
2025-02-24 12:04:57 +0000

    Adding cert

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,6 @@ sudo npm install -g pm2
 pm2 stop example_app
 cd ExampleApplication/
 npm install
+echo $PRIVATE_KEY > privatekey.pem
+echo $SERVER > server.crt
 pm2 start ./bin/www --name example_app
